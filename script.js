ea5924c9eefedc5f5d252ea7833ac1a1e1d0e611
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2024-10-03 11:25:19 -0300

    Criando uma função

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // Estrutura de repetição
-for(let i=0;i <10;i++){
-    console.log(i)
-}
+// for(let i=0;i <10;i++){
+//     console.log(i)
+// }
 
  //WHILE
 
- let f=0;
- while(f < 10){
-    console.log(f)
-    f++;
+//  let f=0;
+//  while(f < 10){
+//     console.log(f)
+//     f++;
 
- }
+//  }
+
+ // While Do
+
+//  let w = 0;
+
+//  do{
+//     console.log(w);
+//     w++;
+//  } while(w<20)
+
+function ola(nome){
+
+    console.log(`Seja bem vindo a ${nome}`)
+    alert(`Seja bem vindo a ${nome}`)
+    document.write(`Seja bem vindo a ${nome}`)
+}
+
+ola("fiap")
